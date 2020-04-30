fba9a4342d781d82ac4cae378ad5fed738120b37
Ryan Kawall <ryankawall01@gmail.com>
2020-04-30 00:29:29 -0400

    Updates

---
+++ precache-manifest.507f63e1daaffed836bdd06a047ebb63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6633c45a89ccf7585cd647c604a20d1",
+    "url": "/covid-19-distribution/index.html"
+  },
+  {
+    "revision": "d999375ac3d0d463ae9f",
+    "url": "/covid-19-distribution/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2a87134f67af881db0c1",
+    "url": "/covid-19-distribution/static/css/main.1736a88a.chunk.css"
+  },
+  {
+    "revision": "d999375ac3d0d463ae9f",
+    "url": "/covid-19-distribution/static/js/2.2ba41af2.chunk.js"
+  },
+  {
+    "revision": "fd8154f77ad230d098e7f14b273e931d",
+    "url": "/covid-19-distribution/static/js/2.2ba41af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a87134f67af881db0c1",
+    "url": "/covid-19-distribution/static/js/main.2b157b7f.chunk.js"
+  },
+  {
+    "revision": "d9675a9e9cd6fd563707",
+    "url": "/covid-19-distribution/static/js/runtime-main.5111eda9.js"
+  }
+]);
