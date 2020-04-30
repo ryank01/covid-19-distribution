60ccdf4c441874c405445295a9c010a2beaf4aa2
Ryan Kawall <ryankawall01@gmail.com>
2020-04-29 22:45:44 -0400

    init
    add package.json
    add src files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CovidData from './components/CovidData';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+  <div>
+      <CovidData />
+  </div>
+  
+  );
+}
+
+export default App;
